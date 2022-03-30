55116a1c347b1d4b2973434f7ed06954fcc235bf
kilru <tim.whitematrix@gmail.com>
2022-03-30 14:42:52 +0800

    feat: use authenticated client for Github interaction

--- pkg/common_test.go
+++ pkg/common_test.go
@@ -1,10 +1,17 @@
 package pkg
 
 import (
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestGetLatestVersion(t *testing.T) {
-	version := GetLatestVersionFromTagName()
-	t.Log(version)
+	_, err := ReadTokenFromS3()
+	if err != nil {
+		t.Log("Errored during testing, this is expected during testing in Github Action if AWS credential is not setup")
+	} else {
+		version := GetLatestVersionFromTagName()
+		assert.NotEmpty(t, version)
+		t.Log(version)
+	}
 }
