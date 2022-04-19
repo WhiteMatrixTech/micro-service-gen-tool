ef494c9070139533ac3acaf833ad6216da9b36f5
kilru <tim.whitematrix@gmail.com>
2022-04-19 11:23:47 +0800

    release: bump up version

--- version/version.go
+++ version/version.go
@@ -2,7 +2,7 @@ package version
 
 import "fmt"
 
-const Version = "v0.1.5"
+const Version = "v0.1.6"
 
 // getVersion Compulsory minimum version, Minimum downward compatibility to this version
 func getVersion() string {
